58f45fc253145091a11228c0d5fcf59e40a8771d
Mykyta Novikov <mykytanovikov@gmail.com>
2018-05-26 13:06:28 +0000

    completed project

--- web/web.py
+++ web/web.py
@@ -1,12 +1,11 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 app = Flask(__name__)
 import boto3, os
 from media.s3_storage import S3MediaStorage
 
 s3 = boto3.resource('s3')
 storage = S3MediaStorage(s3, os.getenv('APP_BUCKET_NAME'))
-
 
 @app.route("/")
 def hello():
@@ -19,15 +18,18 @@ def order_animation():
     invitation="Welcome at ani creatior"
   )
 
-@app.route("/upload", methods=['POST'])
+@app.route("/upload", methods=["POST"])
 def handle_upload():
   if 'my_file' not in request.files:
-    return "something is not yes"
-  my_file = request.files["my_file"]
+    return "something is not yes!!!"
+  my_file = request.files['my_file']
   storage.save(
    path="ab/bc/photo",
    file_to_be_uploaded=my_file
   )
+
+  return "ok"
+
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=True)
